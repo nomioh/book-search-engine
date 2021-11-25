391df61db9f45b06fa9e5dc7410b0e19955c93a7
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-11-25 15:27:03 -0500

    update login form

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
 const db = require("./config/connection");
 const routes = require("./routes");
 
 // Apollo Server connection
-const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
 // import typeDefs & resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -32,7 +31,7 @@ const startServer = async () => {
 
 startServer();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
